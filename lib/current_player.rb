5cff9024c156bb62540450d53fedf2fbf859868a
Emily Morgan <emorgan05@gmail.com>
2017-09-02 18:44:33 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,15 @@ def turn_count(board)
   number_of_turns = 0
   board.each do |board_space|
     if board_space == "X" || board_space == "O"
-      puts number_of_turns += 1
+      number_of_turns += 1
     end
   end
-  puts number_of_turns
 end
 
-
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    current_player = "X"
+  else
+    current_player = "O"
+  end
+end
